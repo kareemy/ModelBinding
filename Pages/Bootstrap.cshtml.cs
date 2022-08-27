251939737e4be2ab18d23ab0e12acde11459b7e2
Kareem Dana <kdana@wtamu.edu>
2022-08-27 12:30:16 -0500

    Update to .NET 6

--- Pages/Bootstrap.cshtml.cs
+++ Pages/Bootstrap.cshtml.cs
@@ -1,44 +1,38 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using System.ComponentModel.DataAnnotations;        // Required using directives for Data Validation
-using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations; // Required using directives for Data Validation
 
-namespace BootstrapDemo.Pages
+namespace ModelBinding.Pages;
+
+public class BootstrapModel : PageModel
 {
-    public class BootstrapModel : PageModel
+    // Model Binding Step 1: Create Property FirstName
+    // Step 2: Add [BindProperty] Decorator
+    [BindProperty]
+    // Add Data Validation Rules
+    [Display(Name = "First Name")]
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
+    public string FirstName {get; set;} = string.Empty;
+
+    [BindProperty]
+    public string LastName {get; set;} = string.Empty;
+
+    private readonly ILogger<BootstrapModel> _logger;
+
+    public BootstrapModel(ILogger<BootstrapModel> logger)
     {
-        // Model Binding Step 1: Create Property FirstName
-        // Step 2: Add [BindProperty] Decorator
-        [BindProperty]
-        // Add Data Validation Rules
-        [Display(Name = "First Name")]
-        [StringLength(60, MinimumLength = 3)]
-        [Required]
-        public string FirstName {get; set;}
+        _logger = logger;
+    }
 
-        [BindProperty]
-        public string LastName {get; set;}
-        private readonly ILogger<BootstrapModel> _logger;
+    public void OnGet()
+    {
 
-        public BootstrapModel(ILogger<BootstrapModel> logger)
-        {
-            _logger = logger;
-        }
+    }
 
-        public void OnGet()
-        {
-
-        }
-
-        public void OnPost()
-        {
-            // Print out FirstName and LastName just like any other property.
-            _logger.LogWarning($"On Post {FirstName} {LastName}");
-        }
+    public void OnPost()
+    {
+        // Print out FirstName and LastName just like any other property.
+        _logger.LogWarning($"OnPost Called {FirstName} {LastName}");
     }
 }
